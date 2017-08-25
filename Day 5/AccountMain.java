44bc5b28639bcd4cf66a916a1f315c8bb16388e0
Akshay Jha <akshayjha@live.in>
2017-08-25 12:24:25 +0530

    Add files via upload

---
+++ Day 5/AccountMain.java
@@ -0,0 +1,144 @@
+import java.util.*;
+class Account
+{
+	String name;
+	int acc_num;
+	String type;
+	double amount,balance;
+	void input()
+	{
+		System.out.println("Enter the customer's name account number and type of account");
+		name=sc.next();
+		acc_num=sc.nextInt();
+		type=sc.next();
+		System.out.println("enter your balance ");
+		double balance=sc.nextDouble();
+	}
+	void deposit()
+	{
+		System.out.println("enter the deposit");
+		double deposit=sc.nextDouble();
+		amount=balance+deposit;
+	}	
+	
+	void display()
+	{
+		System.out.println("customer :"+name+"    "+"account number :"+acc_num+"    "+"type of account "+type);
+		System.out.println("Your total balance is :"+amount);
+	}
+}
+class savings extends Account
+{
+	void disp1()
+	{
+		System.out.println("sorry , we do not provide cheque book facility");
+	}
+	void interest_cal()
+	{
+		double interest;
+		System.out.println("enter the time ");
+		int time=sc.nextInt();	
+		interest=(amount*0.04*time);
+	}	
+	void withdrawal_sv()
+	{
+		System.out.println("enter amount to withdraw");
+		double wdt=sc.nextDouble();
+		amount=amount-wdt;
+		System.out.println("after withdrawal your balance is "+amount);
+		if(amount<5000.0)
+		{
+			System.out.println("your balance is falling below the minimum....rs 150 will be deducted");
+			amount=amount-150.0;
+			System.out.println("youe new balance is :"+amount);
+		}
+	}
+	
+}
+
+class currentac extends Account
+{
+	void disp2()
+	{
+		System.out.println("we do provide cheque book facility: ");
+	}
+	void withdrawal_ac()
+	{
+		System.out.println("enter amount to withdraw");
+		double wdt=sc.nextDouble();
+		amount=amount-wdt;
+		System.out.println("after withdrawal your balance is "+amount);
+		if(amount<5000.0)
+		{
+			System.out.println("your balance is falling below the minimum....rs 150 will be deducted");
+			amount=amount-150.0;
+			System.out.println("youe new balance is :"+amount);
+		}
+	}
+}
+class loan extends Account
+{
+	int tenure;
+	double repayment;
+	double baln;
+	void calc()
+	{	
+		withdraw();
+		if(amount==baln)
+		{
+			System.out.println("monthly payment done ");
+			tenure--;
+			System.out.println("remaining tenures are "+tenure);
+			if(tenure==0)
+			{
+				System.out.println("loan has been cleared");
+			}
+		}
+	}
+	void withdraw()
+	{
+		System.out.println("enter amount to be repayed :");
+		repayment=sc.nextDouble();
+		baln=amount-repayment;
+	}
+}
+public class AccountMain
+{	
+	public static void main(String args[])
+	{
+		BufferedReader sc=new BuferedReader(new InputStreamReader(System.in))
+		int ch;
+		savings sv=new savings();
+		currentac cu=new currentac();
+		loan ln=new loan();
+		do
+		{
+		System.out.println("enter choice 1:savings   2:current   3:loan");
+		ch=Integer.parseInt(sc.readLine());
+		switch (ch)
+		{
+			case 1:
+			{
+				sv.disp1();
+				sv.interest_cal();
+				sv.withdrawal_sv();
+				break;
+			}
+			case 2:
+			{
+				cu.disp2();
+				cu.withdrawal_ac();
+				break;
+			}
+			case 3:
+			{
+				ln.calc();
+				break;
+			}
+			default:
+				System.out.println("invalid choice");
+		}
+		}while(ch==1||ch==2||ch==3);
+	}
+}		
+		
---
+++ Day 5/account.java
@@ -0,0 +1,140 @@
+class account
+{
+	String customer_name;
+	String account_number;
+	String account_type;
+	float balance;
+
+	public void set_name(String name) {customer_name = name;}
+	public void set_acc_number(String acc_number){account_number = acc_number;}
+	public void set_acc_type(String type) {account_type = type;}
+	public void set_balance(float value){balance = value;}
+
+	public String get_name(){return customer_name;}
+	public String get_acc_number(){return account_number;}
+	public String get_acc_type() {return account_type;}
+	public float get_balance(){return balance;}
+
+	
+	public void deposit(float amount)
+	{
+		balance+=amount;
+	}
+
+	public static void main(String args[])
+	{        
+		savings_account s1 = new savings_account();  
+		s1.set_name("Akshay");
+		s1.set_acc_number("SBI20170201");
+		s1.set_acc_type("Savings");
+		s1.set_balance(40000);
+		System.out.println("Customer name: "+ s1.get_name());
+		System.out.println("Account number: "+ s1.get_acc_number());
+		System.out.println("Account type: " + s1.get_acc_type());
+		System.out.println("Current balance: " + s1.get_balance());
+	
+		s1.withdraw(35000);
+		s1.check_balance();
+		s1.deposit(2000);
+		s1.compute_interest(10);
+		s1.check_balance();
+	}
+
+
+}
+
+class savings_account extends account
+{
+	float interest;
+	void compute_interest(int month)
+	{
+		interest = balance + (balance *month* 4/1200);
+		System.out.println("Interest: " + interest);
+		
+	}
+	void withdraw(float x)
+	{			
+		balance -= x;
+	}
+	
+	void check_balance()
+	{
+		if(balance <= 5000)
+		{
+			System.out.println("Account balance too low.");
+			System.out.println("Rs. 150 fine charged. This will be added to your balance");
+			balance -= 150;
+		}
+		System.out.println("Current balance: " + balance);
+	}
+	
+
+}
+class current_account extends account
+{
+	void add_cheque(float amount)
+	{
+		balance += amount;
+	}
+	void withdraw(float amount)
+	{			
+		balance -= amount;
+	}
+	
+	void check_balance()
+	{
+		if(balance < 1000)
+		{
+			System.out.println("Rs. 150 fine charged. This will be added to your balance");
+			balance -= 150;
+		}
+		System.out.println("Current balance: " + balance);
+	}
+
+}
+class loan_account extends account
+{
+	float loan_amount;
+	int tenure;
+	float repayment_value = loan_amount/tenure;
+	void check_repayment(int rd, int rm, int ry, int cd, int cm, int cy)
+	{
+		if(loan_amount == 0)
+		{
+			System.out.println("Loan repayed");
+		}
+		if(ry>cy)
+		{
+			if(rm>cm)
+			{
+				if(rd<cd)
+				{
+					System.out.println("Rs 200 fine imposed");
+					balance -= 200;
+				}
+			}		
+			else 
+			{
+				System.out.println("Rs 200 fine imposed");
+				balance -= 200;
+			}
+		}
+		else 
+		{
+			System.out.println("Rs 200 fine imposed");
+			balance -= 200;
+		}
+		
+	}
+	void repay()
+	{
+		System.out.println("Repayed");
+		loan_amount -= repayment_value;
+		tenure--;
+		if(loan_amount == 0)
+		{
+			System.out.println("Loan repayed");
+		}
+	}
+
+}
