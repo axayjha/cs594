1b2f81b437f2b66657644b79ba75268ae008d64f
Akshay Jha <akshayjha@live.in>
2017-11-23 13:07:59 +0530

    Add files via upload

---
+++ Exam/Swing/eventdemo.java
@@ -0,0 +1,46 @@
+  import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class eventdemo
+{
+	JLabel jlab;
+	eventdemo()
+	{
+		JFrame jfrm=new JFrame("an event example");
+		jfrm.setLayout(new FlowLayout());
+		jfrm.setSize(220,30);
+		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton jbtnAlpha= new JButton("alpha");
+		JButton jbtnBeta= new JButton("beta");
+		jbtnAlpha.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				jlab.setText("alpha was pressed");
+			}
+		});
+		jbtnBeta.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				jlab.setText("beta was pressed");
+			}
+		});
+		jfrm.add(jbtnAlpha);
+		jfrm.add(jbtnBeta);
+		jlab=new JLabel("press a button");
+		jfrm.add(jlab);
+		jfrm.setVisible(true);
+
+	}
+	public static void main(String args[])
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				new eventdemo();
+			}
+		});
+	}
+}
---
+++ Exam/Swing/swingdemo.java
@@ -0,0 +1,23 @@
+ import javax.swing.*;
+class swingdemo
+{
+	swingdemo()
+	{
+		JFrame jfrm=new JFrame("a simple swing app");
+		jfrm.setSize(275,100);
+		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel jlab=new JLabel("Swing means powerful");
+		jfrm.add(jlab);
+		jfrm.setVisible(true);
+	}
+	public static void main(String args[])
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				new swingdemo();
+			}
+		});
+	}
+}
